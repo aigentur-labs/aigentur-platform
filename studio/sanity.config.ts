39ebb9326e81deab2bd016dcea112a0d4baa4e9e
Aigentur <info@aigentur.ch>
2025-11-20 10:33:37 +0100

    Update sanity.config.ts

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
+import {deskStructure} from './deskStructure'
 
 export default defineConfig({
   name: "default",
